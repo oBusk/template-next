8b95b9f083d07d89b6e13ad27e3a876f6644cad3
Oscar Busk <oscar.busk@gmail.com>
2024-03-07 22:46:21 +0100

    Remove unecessary stuff

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // Does not work with appDir
-    // https://beta.nextjs.org/docs/styling/css-in-js
-    // compiler: {
-    //     emotion: true,
-    // },
     eslint: {
         ignoreDuringBuilds: true,
-    },
-    experimental: {
-        serverComponentsExternalPackages: [
-            "libnpmdiff",
-            "npm-package-arg",
-            "pacote",
-        ],
     },
 };
 
