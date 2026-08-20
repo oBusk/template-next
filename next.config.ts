ab69b430906cb57daf34b58081a806516b631e13
Oscar Busk <oscar.busk@gmail.com>
2026-08-21 00:30:17 +0200

    Revert "Enable the Rust React Compiler under Turbopack"
    This reverts commit 8d2f9bb1cf3f2c5a20b47714969f25b1be1beea0.

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     reactCompiler: true,
     cacheComponents: true,
-    experimental: {
-        turbopackRustReactCompiler: true,
-    },
 };
 
 export default nextConfig;
